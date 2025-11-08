1b82260f0db0165ef0e99e84fd472e4d1e056269
BYU-WDD130 <henriquezdery@gmail.com>
2025-11-08 15:24:26 -0500

    chamber

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -3,16 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const cards = document.querySelectorAll(".quick-links .card");
 
   const quickLinkImages = [
-    "image/membership1.jpg",
-    "image/events1(1).jpg",
-    "image/suitcase(1).jpg",
-    "image/resources.jpg"
+    "images/membership1.jpg",
+    "images/events1(1).jpg",
+    "images/suitcase(1).jpg",
+    "images/resources.jpg"
   ];
 
   cards.forEach((card, index) => {
     const img = document.createElement("img");
     img.src = quickLinkImages[index];
-    img.alt = card.textContent + " image";
+    img.alt = card.textContent + " images";
     img.classList.add("card-img");
     card.prepend(img);
   });
@@ -21,20 +21,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const eventCards = document.querySelectorAll(".events .event-card");
 
   const eventImages = [
-    "image/mixernet(1).jpg",
-    "image/workshop(1).jpg",
-    "image/marketing(2).jpg",
-    "image/marketing(2).jpg",
-    "image/marketing(2).jpg",
-    "image/marketing(2).jpg",
-    "image/marketing(2).jpg",
-    "image/marketing(2).jpg"
+    "images/mixernet(1).jpg",
+    "images/workshop(1).jpg",
+    "images/marketing(2).jpg",
+    "images/marketing(2).jpg",
+    "images/marketing(2).jpg",
+    "images/marketing(2).jpg",
+    "images/marketing(2).jpg",
+    "images/marketing(2).jpg"
   ];
 
   eventCards.forEach((eventCard, index) => {
     const img = document.createElement("img");
     img.src = eventImages[index];
-    img.alt = eventCard.querySelector("h3").textContent + " image";
+    img.alt = eventCard.querySelector("h3").textContent + " images";
     img.classList.add("event-img");
     eventCard.prepend(img);
   });
@@ -44,9 +44,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const businessCards = document.querySelectorAll('.featured-businesses .business-card');
   
   const businessImages = [
-    "image/fitness.jpg",
-    "image/Bakery.jpg",
-    "image/downtown.jpg"
+    "images/fitness.jpg",
+    "images/Bakery.jpg",
+    "images/downtown.jpg"
   ];
   businessCards.forEach((businessCard, index) => {
     const img = document.createElement("img");
