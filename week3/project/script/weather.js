7b3262908ef25250dc148e07f3c2f6c2971662f0
BYU-WDD130 <henriquezdery@gmail.com>
2025-11-11 09:07:35 -0500

    weather

--- week3/project/script/weather.js
+++ week3/project/script/weather.js
@@ -32,7 +32,7 @@ function displayResults(data) {
     myTown.textContent = data.name;
     myDescription.innerHTML = data.weather[0].description;
     myTemperature.innerHTML = data.main.temp.toFixed(0);
-    currentTemp.textContent = `${data.main.temp.}&deg;F`
+    currentTemp.textContent = `${data.main.temp.}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}@2x.png`;
     myGraphic.setAttribute('SRC', iconsrc)
     myGraphic.setAttribute('alt', data.weather[0].description)
