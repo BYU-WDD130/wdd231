c4ee586f8cd845022a8a9ed65b5043a2d351b88a
BYU-WDD130 <henriquezdery@gmail.com>
2025-11-11 09:06:34 -0500

    weather

--- week3/project/script/weather.js
+++ week3/project/script/weather.js
@@ -1,15 +1,15 @@
 const myTown = document.querySelector('#town');
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const myDescription = document.querySelector('#description');
+const myTemperature = document.querySelector('#temperature');
+const myGraphic = document.querySelector('graphic');
 
 // Trier, Germany coordinates
 const myKey = f3849d6e217234d7429486bdaab8f5bb;
-const lat = 49.75156;
-const lon = 6.63713;
+const lat = 49.75;
+const lon = 6.63;
 
 // OpenWeatherMap API URL (replace YOUR_API_KEY_HERE with your actual key)
-const myUrl = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${myKey}&unit=imperial`
+const myUrl = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&units=metric&appid=${myKey}&unit=imperial`
 
 async function apiFetch() {
   try {
@@ -26,10 +26,17 @@ async function apiFetch() {
   }
 }
 
-apiFetch();
-
 function displayResults(data) {
     console.log("hello")
 
+    myTown.textContent = data.name;
+    myDescription.innerHTML = data.weather[0].description;
+    myTemperature.innerHTML = data.main.temp.toFixed(0);
+    currentTemp.textContent = `${data.main.temp.}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}@2x.png`;
+    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute('alt', data.weather[0].description)
 
 }
+
+apiFetch();
