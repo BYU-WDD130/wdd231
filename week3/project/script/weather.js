4a413189d0a90a14f977d37833477646fa6c1b62
BYU-WDD130 <henriquezdery@gmail.com>
2025-11-12 10:09:11 -0500

    weather

--- week3/project/script/weather.js
+++ week3/project/script/weather.js
@@ -1,22 +1,23 @@
 const myTown = document.querySelector('#town');
 const myDescription = document.querySelector('#description');
 const myTemperature = document.querySelector('#temperature');
-const myGraphic = document.querySelector('#graphic');
+const myGraphic = document.querySelector('#graphic'); 
 
 // Trier, Germany coordinates
-const myKey = 'f3849d6e217234d7429486bdaab8f5bb';
+const myKey = 'f3849d6e217234d7429486bdaab8f5bb'; 
 const lat = 49.75;
 const lon = 6.63;
 
-// OpenWeatherMap API URL (replace YOUR_API_KEY_HERE with your actual key)
-const myUrl = `http//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&units=metric&appid=&unit=imperial=${myKey}`;
+// OpenWeatherMap API URL
+const myUrl = `http//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${myKey}`;
+
 
 async function apiFetch() {
   try {
     const response = await fetch(myUrl);
     if (response.ok) {
       const data = await response.json();
-      console.log(data); // testing only
+      console.log(data); 
       displayResults(data);
     } else {
       throw Error(await response.text());
@@ -27,16 +28,13 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-    console.log("hello")
+  myTown.textContent = data.name;
+  myDescription.textContent = data.weather[0].description;
+  myTemperature.innerHTML = `${data.main.temp.toFixed(0)}°F`;
 
-    myTown.textContent = data.name;
-    myDescription.innerHTML = data.weather[0].description;
-    myTemperature.innerHTML = data.main.temp.toFixed(0);
-    currentTemp.textContent = `${data.main.temp.toFixed(0)}°F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}@2x.png`;
-    myGraphic.setAttribute('SRC', iconsrc)
-    myGraphic.setAttribute('alt', data.weather[0].description);
-
+  const iconSrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  myGraphic.setAttribute('src', iconSrc);
+  myGraphic.setAttribute('alt', data.weather[0].description);
 }
 
 apiFetch();
