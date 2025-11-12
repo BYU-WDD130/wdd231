8fb694864b88072143de45cf993e75be7ac24a77
BYU-WDD130 <henriquezdery@gmail.com>
2025-11-12 10:18:36 -0500

    weather

--- week3/project/script/weather.js
+++ week3/project/script/weather.js
@@ -6,15 +6,15 @@ const myGraphic = document.querySelector('#graphic');
 // Trier, Germany coordinates
 const myKey = 'f3849d6e217234d7429486bdaab8f5bb'; 
 const lat = 49.75;
-const lon = 6.63;
+const long = 6.63;
 
 // OpenWeatherMap API URL
-const myUrl = `http//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${myKey}`;
+const myUrl = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&APPID=${myKey}&units=imperial&appid=`;
 
 
 async function apiFetch() {
   try {
-    const response = await fetch(Url);
+    const response = await fetch(myUrl);
     if (response.ok) {
       const data = await response.json();
       console.log(data); 
@@ -33,7 +33,7 @@ function displayResults(data) {
   myTemperature.innerHTML = `${data.main.temp.toFixed(0)}°F`;
 
   const iconSrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  myGraphic.setAttribute('src', iconSrc);
+  myGraphic.setAttribute('SRC', iconSrc);
   myGraphic.setAttribute('alt', data.weather[0].description);
 }
 
