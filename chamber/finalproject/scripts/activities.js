1ad0bc228d7d367993f886f042e601602ad108dd
BYU-WDD130 <henriquezdery@gmail.com>
2025-12-09 15:18:58 -0500

    project

--- chamber/finalproject/scripts/activities.js
+++ chamber/finalproject/scripts/activities.js
@@ -35,10 +35,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Última modificación
+document.getElementById("LastModified").textContent =
+  "Last Modified: " + document.lastModified;
+
+// NAV hamburguesa
 const navbutton = document.querySelector('#ham-btn');
 const navBar = document.querySelector('#nav-bar');
 
 navbutton.addEventListener('click', () => {
   navbutton.classList.toggle('show');
-  navBar.classList.toggle('show');  
+  navBar.classList.toggle('show');
 });
--- chamber/finalproject/scripts/index.js
+++ chamber/finalproject/scripts/index.js
@@ -1,9 +1,16 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Última modificación
+document.getElementById("LastModified").textContent =
+  "Last Modified: " + document.lastModified;
+
+// NAV hamburguesa
 const navbutton = document.querySelector('#ham-btn');
 const navBar = document.querySelector('#nav-bar');
 
 navbutton.addEventListener('click', () => {
   navbutton.classList.toggle('show');
-  navBar.classList.toggle('show');  
+  navBar.classList.toggle('show');
 });
 
 
